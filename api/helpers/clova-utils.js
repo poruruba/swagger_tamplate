d3f42c4f87cfa013901114f13a049c2be1aec540
poruruba <44470781+poruruba@users.noreply.github.com>
2019-02-10 23:32:19 +0900

    Update clova-utils.js

--- api/helpers/clova-utils.js
+++ api/helpers/clova-utils.js
@@ -5,24 +5,37 @@ class ClovaUtils{
         this.clova = clova;
         this.clovaSkillHandler = clova.Client.configureSkill();
         this.launchHandle = null;
+        this.eventHandle = null;
         this.sessionEndedHandle = null;
         this.intentHandles = new Map();
 
         this.clovaSkillHandler
         .onLaunchRequest(async responseHelper => {
-            if( this.launchHandle )
+            if( this.launchHandle ){
+                console.log('handle: LaunchRequest called');
                 return await this.launchHandle(responseHelper);
+            }
         })
         .onIntentRequest(async responseHelper => {
             const intent = responseHelper.getIntentName();
 
             var handle = this.intentHandles.get(intent);
-            if( handle )
+            if( handle ){
+                console.log('handle: ' + intent + ' called');
                 return await handle(responseHelper);
+            }
         })
         .onSessionEndedRequest(async responseHelper => {
-            if( this.sessionEndedHandle )
+            if( this.sessionEndedHandle ){
+                console.log('handle: SessionEndedRequest called');
                 return await this.sessionEndedHandle(responseHelper);
+            }
+        })
+        .onEventRequest(async responseHelper => {
+            if( this.eventHandle ){
+                console.log('handle: EventRequest called');
+                return await this.eventdHandle(responseHelper);
+            }
         });
     }
 
@@ -31,6 +44,8 @@ class ClovaUtils{
             this.launchHandle = handle;
         else if( matcher == 'SessionEndedRequest')
             this.sessionEndedHandle = handle;
+        else if( matcher == 'EventRequest')
+            this.eventHandle = handle;
         else
             this.intentHandles.set(matcher, handle);
     }
@@ -49,6 +64,10 @@ class ClovaUtils{
 
     handle(){
         return this.clovaSkillHandler.handle();
+    }
+
+    lambda(){
+        return this.clovaSkillHandler.lambda();
     }
 };
 
