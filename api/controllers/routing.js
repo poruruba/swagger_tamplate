bcf925ad17d19536d28d8e89fea82235af058d3a
poruruba <44470781+poruruba@users.noreply.github.com>
2020-01-04 12:59:01 +0900

    Update routing.js

--- api/controllers/routing.js
+++ api/controllers/routing.js
@@ -1,30 +1,6 @@
 'use strict';
 
-/* 関数を以下に追加する */
-const func_table = {
-//  "test-func" : require('./test_func').handler,
-//  "test-dialogflow" : require('./test_dialogflow').fulfillment,
-"test-get" : require('./test-get').handler,
-"test-post" : require('./test-post').handler,
-};
-const alexa_table = {
-//  "test-alexa" : require('./test_alexa').handler,
-//  "test-clova": require('./test-clova').handler,
-};
-const lambda_table = {
-//  "test-lambda" : require('./test-lambda').handler,
-//  "forward_lambda" : require('./forward_lambda').handler,
-};
-const express_table = {
-//  "test-express": require('./test-express').handler,
-};
-/* ここまで */
-
-/* 必要に応じて、バイナリレスポンスのContent-Typeを以下に追加する */
-const binary_table = [
-//  'application/octet-stream',
-];
-/* ここまで */
+const { func_table, alexa_table, lambda_table, express_table, binary_table } = require('./functions.js');
 
 var exports_list = {};
 for( var operationId in func_table ){
